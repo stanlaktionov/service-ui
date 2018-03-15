7eb9dff0845b75644c61b9c0e1922531ac2f5e34
Maxim Tumas <DiscoElevator@users.noreply.github.com>
2018-03-15 15:45:17 +0300

    EPMRPP-33167: Add column sorting Add short title

--- app/src/components/main/grid/gridHeader/gridHeader.jsx
+++ app/src/components/main/grid/gridHeader/gridHeader.jsx
@@ -1,48 +1,40 @@
 import PropTypes from 'prop-types';
 import classNames from 'classnames/bind';
-import { ALIGN_CENTER, ALIGN_LEFT, ALIGN_RIGHT } from '../constants';
+import { columnPropTypes } from '../propTypes';
+import { HeaderCell } from './headerCell';
 import styles from './gridHeader.scss';
 
 const cx = classNames.bind(styles);
 
-export const GridHeader = ({ columns }) => (
+export const GridHeader = ({ columns, sortingColumn, sortingDirection, onChangeSorting }) => (
   <div className={cx('grid-header')}>
     {
       columns.map((column, i) =>
         (
           <HeaderCell
-            key={i}
+            key={name || i}
             title={column.title}
-            width={column.width}
             align={column.align}
+            sortable={column.sortable}
+            name={column.name}
+            sortingDirection={sortingDirection}
+            sortingActive={sortingColumn === column.name}
+            onChangeSorting={onChangeSorting}
           />
         ))
     }
   </div>
 );
 GridHeader.propTypes = {
-  columns: PropTypes.arrayOf(PropTypes.shape({
-    title: PropTypes.node,
-    width: PropTypes.string,
-    align: PropTypes.oneOf([ALIGN_LEFT, ALIGN_CENTER, ALIGN_RIGHT]),
-  })),
+  columns: columnPropTypes,
+  sortingColumn: PropTypes.string,
+  sortingDirection: PropTypes.string,
+  onChangeSorting: PropTypes.func,
 };
 GridHeader.defaultProps = {
   columns: [],
-};
-
-const HeaderCell = ({ title, width, align }) => (
-  <div className={cx('grid-header-cell', { [`align-${align}`]: align })} style={{ width }}>
-    {title}
-  </div>
-);
-HeaderCell.propTypes = {
-  title: PropTypes.node,
-  width: PropTypes.string,
-  align: PropTypes.oneOf([ALIGN_LEFT, ALIGN_CENTER, ALIGN_RIGHT]),
-};
-HeaderCell.defaultProps = {
-  title: '',
-  width: 'auto',
-  align: ALIGN_LEFT,
+  sortingColumn: '',
+  sortingDirection: null,
+  onChangeSorting: () => {
+  },
 };
--- app/src/components/main/grid/propTypes.js
+++ app/src/components/main/grid/propTypes.js
@@ -2,8 +2,13 @@ import PropTypes from 'prop-types';
 import { ALIGN_CENTER, ALIGN_LEFT, ALIGN_RIGHT } from './constants';
 
 export const columnPropTypes = PropTypes.shape({
-  title: PropTypes.node,
+  title: PropTypes.shape({
+    full: PropTypes.string,
+    short: PropTypes.string,
+  }),
   component: PropTypes.oneOfType([PropTypes.node, PropTypes.func]),
   align: PropTypes.oneOf([ALIGN_LEFT, ALIGN_CENTER, ALIGN_RIGHT]),
   formatter: PropTypes.func,
+  sortable: PropTypes.bool,
+  name: PropTypes.string,
 });
